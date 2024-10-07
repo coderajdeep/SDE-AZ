b10c57f9d70982c053854aba814788520bc483ab
Rajdeep Mallick <coder.rajdeep@gmail.com>
2024-10-07 18:19:20 +0530

    Update string problem

--- string/03--largestOddNumber--1903.cpp
+++ string/03--largestOddNumber--1903.cpp
@@ -3,18 +3,17 @@
 // Space complexity O(1)
 // Here we are modifying the given string
 string largestOddNumber(string num) {
-    int n = num.size();
-    for(int i=n-1; i>=0; --i) {
-        if((num[i]-'0') & 1) {
+    while(!num.empty()) {
+        if((num.back()-'0') & 1) {
             return num;
         }
         num.pop_back();
     }
-    return "";
+    return num;
 }
 // solution 2
 // Time complexity O(n)
-// Space complexity O(n)
+// Space complexity O(n) [as substring will create another string]
 string largestOddNumber(string num) {
     int n = num.size();
     for(int i=n-1; i>=0; --i) {
