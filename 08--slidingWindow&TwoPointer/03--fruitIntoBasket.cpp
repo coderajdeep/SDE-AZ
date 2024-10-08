b48e7bf35e95a4581ec6dd4e3a020311ed3172d3
Rajdeep Mallick <coder.rajdeep@gmail.com>
2024-10-08 17:47:15 +0530

    Fruits into basket

---
+++ 08--slidingWindow&TwoPointer/03--fruitIntoBasket.cpp
@@ -0,0 +1,22 @@
+// Time complexity O(n)
+// Space complexity O(n)
+class Solution {
+  public:
+    int totalFruits(vector<int> &arr) {
+        int n = arr.size(), left = 0, right = 0, total = 0;
+        unordered_map<int, int> umap;
+        while(right<n) {
+            umap[arr[right]]++;
+            while(left<=right && umap.size()>2) {
+                umap[arr[left]]--;
+                if(umap[arr[left]]==0) {
+                    umap.erase(arr[left]);
+                }
+                ++left;
+            }
+            total = max(total, right-left+1);
+            ++right;
+        }
+        return total;
+    }
+};
