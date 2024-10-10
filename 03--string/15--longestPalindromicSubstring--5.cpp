1bb443f12de241f934ffd86830cfe6e9f25077ac
Rajdeep Mallick <coder.rajdeep@gmail.com>
2024-10-11 02:09:03 +0530

    Update longest palindromic substring

--- 03--string/15--longestPalindromicSubstring--5.cpp
+++ 03--string/15--longestPalindromicSubstring--5.cpp
@@ -34,3 +34,46 @@ public:
         return s.substr(start, maxLen);
     }
 };
+
+// Solution 3
+// Dynamic programming solution
+// Time complexity O(n*n)
+// Space complexity O(n*n) 
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size(), maxLen = 1, start = 0;
+        bool dp[n][n];
+        dp[0][0] = true;
+        for(int i=1; i<n; ++i) {
+            dp[i][i] = true;
+            if(s[i-1]==s[i]) {
+                dp[i-1][i] = true;
+                if(maxLen < 2) {
+                    start = i-1;
+                    maxLen = 2;
+                }
+            }
+            else {
+                dp[i-1][i] = false;
+            }
+        }
+        int col = 2;
+        while(col<n) {
+            int i = 0;
+            int j = col;
+            while(i<n && j<n) {
+                if(s[i]==s[j]) dp[i][j] = dp[i+1][j-1];
+                else dp[i][j] = false;
+                if(dp[i][j] && maxLen < (j-i+1)) {
+                    start = i;
+                    maxLen = (j-i+1);
+                }
+                ++i;
+                ++j;
+            }
+            ++col;
+        }
+        return s.substr(start, maxLen);
+    }
+};
