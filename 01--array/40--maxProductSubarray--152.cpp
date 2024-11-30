67b9be7f249a9e80bb4c6707efc5aba6b01e33b5
coderajdeep <coder.rajdeep@gmail.com>
2024-11-30 09:08:22 +0530

    Max product subarray

--- 01--array/40--maxProductSubarray--152.cpp
+++ 01--array/40--maxProductSubarray--152.cpp
@@ -2,6 +2,22 @@
 // Time complexity O(n)
 // Space complexity O(1)
 
+// Single traversal
+int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    int prefix = 1, suffix = 1, ans = INT_MIN;
+    for(int i=0; i<n; ++i) {
+        prefix = (prefix * nums[i]);
+        ans = max(ans, prefix);
+        suffix = (suffix * nums[n-1-i]);
+        ans = max(ans, suffix);
+        if(prefix==0) prefix = 1;
+        if(suffix==0) suffix = 1;
+    }
+    return ans;
+}
+
+// two traversal
 int maxProduct(vector<int>& nums) {
     int n = nums.size(), ans = INT_MIN;
     int prefix = 1;
@@ -28,3 +44,20 @@ int maxProduct(vector<int>& nums) {
     }
     return ans;
 }
+
+// Brute force
+// Time complexity O(n*n)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = INT_MIN, n = nums.size();
+        for(int i=0; i<n; ++i) {
+            int product = 1;
+            for(int j=i; j<n; ++j) {
+                product = (product * nums[j]);
+                ans = max(ans, product);
+            }
+        } 
+        return ans;
+    }
+};
