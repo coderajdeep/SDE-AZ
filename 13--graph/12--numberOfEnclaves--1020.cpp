5e6028d7d5ae317c00f7dc95de16460f431de66c
coderajdeep <coder.rajdeep@gmail.com>
2025-02-18 15:58:49 +0530

    Number of enclaves

--- 13--graph/12--numberOfEnclaves--1020.cpp
+++ 13--graph/12--numberOfEnclaves--1020.cpp
@@ -53,3 +53,48 @@ public:
         return count;
     }
 };
+
+// DFS solution
+// Time complexity O(n*m)
+// Space complexity O(n*m)
+class Solution {
+private:
+    void dfs(int x, int y, int n, int m, vector<vector<int>>& grid) {
+        if(x<0 || x>=n || y<0 || y>=m || (grid[x][y] == 0)) return;
+        grid[x][y] = 0;
+        dfs(x+1, y, n, m, grid);
+        dfs(x-1, y, n, m, grid);
+        dfs(x, y+1, n, m, grid);
+        dfs(x, y-1, n, m, grid);
+    }    
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0; i<n; ++i) {
+            if(grid[i][0]) {
+                dfs(i, 0, n, m, grid);
+            }
+            if(grid[i][m-1]) {
+                dfs(i, m-1, n, m, grid);
+            }
+        }
+        for(int i=0; i<m; ++i) {
+            if(grid[0][i]) {
+                dfs(0, i, n, m, grid);
+            }
+            if(grid[n-1][i]) {
+                dfs(n-1, i, n, m, grid);
+            }
+        }
+        int count = 0;
+        for(int i=1; i<n-1; ++i) {
+            for(int j=1; j<m-1; ++j) {
+                if(grid[i][j]) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
